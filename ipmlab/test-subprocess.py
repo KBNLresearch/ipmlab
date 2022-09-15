3523ed0d342ce5675ed8274df22bb10b7089bb0e
bitsgalore <johan.vanderknijff@kb.nl>
2022-09-15 16:34:21 +0200

    added GUI code

--- ipmlab/test-subprocess.py
+++ ipmlab/test-subprocess.py
@@ -1,36 +1,117 @@
 #! /usr/bin/env python3
-
-import os
-import io
+import time
+import threading
 import platform
+import logging
 import subprocess as sub
+import tkinter as tk
+import tkinter.scrolledtext as ScrolledText
 
-"""Extract data to disk image"""
 
-# Aaru binary
-aaruBin = "/usr/local/bin/aaru"
+class TextHandler(logging.Handler):
+    # This class allows you to log to a Tkinter Text or ScrolledText widget
+    # Adapted from Moshe Kaplan: https://gist.github.com/moshekaplan/c425f861de7bbf28ef06
+    
+    def __init__(self, text):
+        # run the regular Handler __init__
+        logging.Handler.__init__(self)
+        # Store a reference to the Text it will log to
+        self.text = text
 
-# Input device
-inDevice = "/dev/sdd"
+    def emit(self, record):
+        msg = self.format(record)
+        def append():
+            self.text.configure(state='normal')
+            self.text.insert(tk.END, msg + '\n')
+            self.text.configure(state='disabled')
+            # Autoscroll to the bottom
+            self.text.yview(tk.END)
+        # This is necessary because we can't modify the Text from other threads
+        self.text.after(0, append)
 
-# Image file name
-imageFile = 'test-aaru.img'
+class myGUI(tk.Frame):
 
-args = [aaruBin]
-args.append("media")
-args.append("dump")
-args.append("--encoding")
-args.append("utf-8")
-args.append("--metadata")
-if platform.system() == "Windows":
-    args.append("".join([inDevice, ":"]))
-elif platform.system() == "Linux":
-    args.append(inDevice)
-args.append(imageFile)
+    # This class defines the graphical user interface 
+    
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.root = parent
+        self.build_gui()
+        
+    def build_gui(self):                    
+        # Build GUI
+        self.root.title('TEST')
+        self.root.option_add('*tearOff', 'FALSE')
+        self.grid(column=0, row=0, sticky='ew')
+        self.grid_columnconfigure(0, weight=1, uniform='a')
+        self.grid_columnconfigure(1, weight=1, uniform='a')
+        self.grid_columnconfigure(2, weight=1, uniform='a')
+        self.grid_columnconfigure(3, weight=1, uniform='a')
+        
+        # Add text widget to display logging info
+        st = ScrolledText.ScrolledText(self, state='disabled')
+        st.configure(font='TkFixedFont')
+        st.grid(column=0, row=1, sticky='w', columnspan=4)
 
-if platform.system() == "Linux":
-    # Unmount input device
-    sub.run(['umount', inDevice], shell=False)
+        # Create textLogger
+        text_handler = TextHandler(st)
+        
+        # Logging configuration
+        logging.basicConfig(filename='test.log',
+            level=logging.INFO, 
+            format='%(asctime)s - %(levelname)s - %(message)s')        
+        
+        # Add the handler to logger
+        logger = logging.getLogger()        
+        logger.addHandler(text_handler)
 
-# Run Aaru as subprocess
-p = sub.run(args, shell=False)
+def worker():
+    # Skeleton worker function, runs in separate thread (see below)   
+    while True:
+        # Report time / date at 2-second intervals
+        time.sleep(2)
+        timeStr = time.asctime()
+        msg = 'Current time: ' + timeStr
+        logging.info(msg)
+
+def dump():
+    # Aaru binary
+    aaruBin = "/usr/local/bin/aaru"
+
+    # Input device
+    inDevice = "/dev/sdd"
+
+    # Image file name
+    imageFile = 'test-aaru.img'
+
+    args = [aaruBin]
+    args.append("media")
+    args.append("dump")
+    args.append("--encoding")
+    args.append("utf-8")
+    args.append("--metadata")
+    if platform.system() == "Windows":
+        args.append("".join([inDevice, ":"]))
+    elif platform.system() == "Linux":
+        args.append(inDevice)
+    args.append(imageFile)
+
+    if platform.system() == "Linux":
+        # Unmount input device
+        sub.run(['umount', inDevice], shell=False)
+
+    # Run Aaru as subprocess
+    p = sub.run(args, shell=False)
+
+def main():
+    
+    root = tk.Tk()
+    myGUI(root)
+    
+    t1 = threading.Thread(target=dump, args=[])
+    t1.start()
+        
+    root.mainloop()
+    t1.join()
+    
+main()
