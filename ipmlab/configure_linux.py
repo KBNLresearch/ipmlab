de6791ce422a63230adf020e37abdb5b118cb932
bitsgalore <johan.vanderknijff@kb.nl>
2022-11-15 16:29:31 +0100

    removed gui stuf (now for real)

--- ipmlab/configure_linux.py
+++ ipmlab/configure_linux.py
@@ -6,17 +6,13 @@ import sys
 import io
 import site
 from shutil import copyfile
-import threading
 import logging
-import tkinter as tk
-import tkinter.scrolledtext as ScrolledText
-import tkinter.messagebox as tkMessageBox
 
 
-def errorExit(error):
-    """Show error message in messagebox and then exit after userv presses OK"""
-    tkMessageBox.showerror("Error", error)
-    os._exit(0)
+def errorExit(msg):
+    """Show error message and exit"""
+    sys.stderr.write("Error: " + msg + "\n")
+    sys.exit(1)
 
 
 def writeDesktopFiles(applicationsDir):
@@ -217,82 +213,17 @@ def post_install():
 
     writeDesktopFiles(applicationsDir)
 
-    msg = 'Ipmlab configuration completed successfully, click OK to exit!'
-    tkMessageBox.showinfo("Info", msg)
-    os._exit(0)
-
-
-class TextHandler(logging.Handler):
-    """This class allows you to log to a Tkinter Text or ScrolledText widget
-    Adapted from Moshe Kaplan:
-    https://gist.github.com/moshekaplan/c425f861de7bbf28ef06
-    """
-
-    def __init__(self, text):
-        # run the regular Handler __init__
-        logging.Handler.__init__(self)
-        # Store a reference to the Text it will log to
-        self.text = text
-
-    def emit(self, record):
-        msg = self.format(record)
-
-        def append():
-            self.text.configure(state='normal')
-            self.text.insert(tk.END, msg + '\n')
-            self.text.configure(state='disabled')
-            # Autoscroll to the bottom
-            self.text.yview(tk.END)
-        # This is necessary because we can't modify the Text from other threads
-        self.text.after(0, append)
-
-
-class myGUI(tk.Frame):
-
-    """This class defines the graphical user interface"""
-
-    def __init__(self, parent, *args, **kwargs):
-        tk.Frame.__init__(self, parent, *args, **kwargs)
-        self.root = parent
-        self.build_gui()
-
-    def build_gui(self):
-        # Build GUI
-        self.root.title('Ipmlab Configuration Tool')
-        self.root.option_add('*tearOff', 'FALSE')
-        self.grid(column=0, row=0, sticky='ew')
-        self.grid_columnconfigure(0, weight=1, uniform='a')
-        self.grid_columnconfigure(1, weight=1, uniform='a')
-        self.grid_columnconfigure(2, weight=1, uniform='a')
-        self.grid_columnconfigure(3, weight=1, uniform='a')
-
-        # Add text widget to display logging info
-        st = ScrolledText.ScrolledText(self, state='disabled')
-        st.configure(font='TkFixedFont')
-        st.grid(column=0, row=1, sticky='w', columnspan=4)
-
-        # Create textLogger
-        text_handler = TextHandler(st)
-
-        # Logging configuration
-        logging.basicConfig(level=logging.INFO,
-                            format='%(asctime)s - %(levelname)s - %(message)s')
-
-        # Add the handler to logger
-        logger = logging.getLogger()
-        logger.addHandler(text_handler)
+    sys.stdout.write('Ipmlab configuration completed successfully!\n')
+    sys.exit(0)
 
 
 def main():
     """Main function"""
-    root = tk.Tk()
-    myGUI(root)
 
-    t1 = threading.Thread(target=post_install, args=[])
-    t1.start()
-
-    root.mainloop()
-    t1.join()
+    # Logging configuration
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+    post_install()
 
 
 if __name__ == "__main__":
