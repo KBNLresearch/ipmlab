5476193ef07296abd1615162a97b89c3fb805246
bitsgalore <johan.vanderknijff@kb.nl>
2022-11-15 20:04:14 +0100

    bumped rc number

--- ipmlab/ipmlab.py
+++ ipmlab/ipmlab.py
@@ -29,7 +29,7 @@ from .kbapi import sru
 from .socketserver import server
 from . import pmworker
 
-__version__ = '0.2.0rc9'
+__version__ = '0.2.0rc10'
 config.version = __version__
 
 class carrierEntry(tk.Frame):
