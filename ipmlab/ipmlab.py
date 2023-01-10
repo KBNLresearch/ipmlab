044ad2d7a1a4ff932d6bf8cfe98bf50788110281
bitsgalore <johan.vanderknijff@kb.nl>
2023-01-10 12:18:31 +0100

    bumped version to 0.3.0a1

--- ipmlab/ipmlab.py
+++ ipmlab/ipmlab.py
@@ -27,7 +27,7 @@ from .kbapi import sru
 from .socketserver import server
 from . import pmworker
 
-__version__ = '0.2.0'
+__version__ = '0.3.0a1'
 config.version = __version__
 
 class carrierEntry(tk.Frame):
